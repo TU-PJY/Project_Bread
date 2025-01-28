2dfc30e3c83c9b9a621297841f75bddfe7e8f525
TU-PJY <viska0319@icloud.com>
2025-01-28 21:26:43 +0900

    빌드 파일 이름 변경

--- mata_SDK/Src/Config.h
+++ mata_SDK/Src/Config.h
@@ -3,7 +3,7 @@
 
 //////////////////////////////////// Window options
 // window name
-constexpr const char* WINDOW_NAME = "mata_SDK";
+constexpr const char* WINDOW_NAME = "Rythm Bread";
 
 // window size
 constexpr int WINDOW_WIDTH = 1200;
